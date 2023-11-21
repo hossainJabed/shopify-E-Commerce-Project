cbf7ac44f0fec5970e920e88ac812c4d64ef20e2
hossainjabed <hossainjabed610@gmail.com>
2023-11-21 10:26:47 +0600

    admin dashboard

---
+++ resources/views/include/sidebar.blade.php
@@ -0,0 +1,110 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li>
+                    <a class="" href="{{route('home')}}" aria-expanded="false">
+                        <i class="icon-speedometer"></i><span class="hide-menu">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-layout-grid2"></i><span class="hide-menu">Category Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="app-calendar.html">Create Category</a></li>
+                        <li><a href="app-chat.html">List Category</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-email"></i><span class="hide-menu">Sub Category</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="app-email.html">Create Subcategory</a></li>
+                        <li><a href="app-email-detail.html">List Subcategory</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-palette"></i>
+                        <span class="hide-menu">Brand Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="ui-cards.html">Create Brand</a></li>
+                        <li><a href="ui-user-card.html">List Brand</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-layout-media-right-alt"></i><span class="hide-menu">Unit Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="form-basic.html">Create Unit</a></li>
+                        <li><a href="form-layout.html">List Unit</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-layout-accordion-merged">
+                        </i><span class="hide-menu">Product Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="table-basic.html">Create Product</a></li>
+                        <li><a href="table-layout.html">List Product</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-settings"></i><span class="hide-menu">Order Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="widget-apps.html">List Order</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-gallery"></i><span class="hide-menu">Customer Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="layout-single-column.html">Create Customer</a></li>
+                        <li><a href="layout-fix-header.html">List Customer</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="ti-files">
+
+                        </i><span class="hide-menu">User Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="starter-kit.html">Crate User</a></li>
+                        <li><a href="pages-blank.html">List User</a></li>
+
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-pie-chart"></i><span class="hide-menu">Coupon Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="chart-morris.html">Create Coupon</a></li>
+                        <li><a href="chart-chartist.html">List Coupon</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
+                        <i class="ti-light-bulb"></i><span class="hide-menu">Setting Module</span>
+                    </a>
+                    <ul aria-expanded="false" class="collapse">
+                        <li><a href="icon-material.html">Company Setting</a></li>
+                        <li><a href="icon-fontawesome.html">Fax Setting</a></li>
+                        <li><a href="icon-fontawesome.html">Shipping Setting</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url('admin')}}/assets/images/favicon.png">
+    <title>Elite Admin Template - The Ultimate Multipurpose admin template</title>
+    <!-- This page CSS -->
+    <!-- chartist CSS -->
+    <link href="{{url('admin')}}/assets/node_modules/morrisjs/morris.css" rel="stylesheet">
+    <!--Toaster Popup message CSS -->
+    <link href="{{url('admin')}}/assets/node_modules/toast-master/css/jquery.toast.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{url('admin')}}/dist/css/style.min.css" rel="stylesheet">
+    <!-- Dashboard 1 Page CSS -->
+    <link href="{{url('admin')}}/dist/css/pages/dashboard1.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body class="skin-blue fixed-layout">
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<div class="preloader">
+    <div class="loader">
+        <div class="loader__figure"></div>
+        <p class="loader__label">Elite admin</p>
+    </div>
+</div>
+<!-- ============================================================== -->
+<!-- Main wrapper - style you can find in pages.scss -->
+<!-- ============================================================== -->
+<div id="main-wrapper">
+    <!-- ============================================================== -->
+    @include('include.header')
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+
+    @include('include.sidebar')
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper  -->
+    <!-- ============================================================== -->
+    <div class="page-wrapper">
+        <!-- ============================================================== -->
+        <!-- Container fluid  -->
+        <!-- ============================================================== -->
+        <div class="container-fluid">
+            @yield('body')
+            <div class="right-sidebar">
+                <div class="slimscrollright">
+                    <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span> </div>
+                    <div class="r-panel-body">
+                        <ul id="themecolors" class="m-t-20">
+                            <li><b>With Light sidebar</b></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme">1</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme working">4</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-purple" class="purple-theme">5</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-megna" class="megna-theme">6</a></li>
+                            <li class="d-block m-t-30"><b>With Dark sidebar</b></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-default-dark" class="default-dark-theme ">7</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-green-dark" class="green-dark-theme">8</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-red-dark" class="red-dark-theme">9</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-blue-dark" class="blue-dark-theme">10</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-purple-dark" class="purple-dark-theme">11</a></li>
+                            <li><a href="javascript:void(0)" data-skin="skin-megna-dark" class="megna-dark-theme ">12</a></li>
+                        </ul>
+                        <ul class="m-t-20 chatonline">
+                            <li><b>Chat option</b></li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>Varun Dhavan <small class="text-success">online</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/2.jpg" alt="user-img" class="img-circle"> <span>Genelia Deshmukh <small class="text-warning">Away</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/3.jpg" alt="user-img" class="img-circle"> <span>Ritesh Deshmukh <small class="text-danger">Busy</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/4.jpg" alt="user-img" class="img-circle"> <span>Arijit Sinh <small class="text-muted">Offline</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/5.jpg" alt="user-img" class="img-circle"> <span>Govinda Star <small class="text-success">online</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/6.jpg" alt="user-img" class="img-circle"> <span>John Abraham<small class="text-success">online</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/7.jpg" alt="user-img" class="img-circle"> <span>Hritik Roshan<small class="text-success">online</small></span></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)"><img src="{{url('admin')}}/assets/images/users/8.jpg" alt="user-img" class="img-circle"> <span>Pwandeep rajan <small class="text-success">online</small></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Right sidebar -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Container fluid  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Page wrapper  -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- footer -->
+    <!-- ============================================================== -->
+    <footer class="footer">
+        @ {{date('y-m-d')}}2021 Desigin & Develop by
+        <a href="https://www.reflexsoft.com/">Reflex Soft.Ltd</a>
+    </footer>
+    <!-- ============================================================== -->
+    <!-- End footer -->
+    <!-- ============================================================== -->
+</div>
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="{{url('admin')}}/assets/node_modules/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{url('admin')}}/assets/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="{{url('admin')}}/dist/js/perfect-scrollbar.jquery.min.js"></script>
+<!--Wave Effects -->
+<script src="{{url('admin')}}/dist/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="{{url('admin')}}/dist/js/sidebarmenu.js"></script>
+<!--Custom JavaScript -->
+<script src="{{url('admin')}}/dist/js/custom.min.js"></script>
+<!-- ============================================================== -->
+<!-- This page plugins -->
+<!-- ============================================================== -->
+<!--morris JavaScript -->
+<script src="{{url('admin')}}/assets/node_modules/raphael/raphael-min.js"></script>
+<script src="{{url('admin')}}/assets/node_modules/morrisjs/morris.min.js"></script>
+<script src="{{url('admin')}}/assets/node_modules/jquery-sparkline/jquery.sparkline.min.js"></script>
+<!-- Popup message jquery -->
+<script src="{{url('admin')}}/assets/node_modules/toast-master/js/jquery.toast.js"></script>
+<!-- Chart JS -->
+<script src="{{url('admin')}}/dist/js/dashboard1.js"></script>
+<script src="{{url('admin')}}/assets/node_modules/toast-master/js/jquery.toast.js"></script>
+</body>
+</html>
+
